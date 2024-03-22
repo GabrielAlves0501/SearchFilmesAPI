8fecc77dbc92387f3b842f708656a5a81bf13dab
Gabriel Alves <73733246+GabrielAlves0501@users.noreply.github.com>
2024-03-22 14:23:13 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
  // Sua chave de API do TheMovieDB
  const apiKey = '43949e7f9533a2681109186af34e4c74';
+ const acessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Mzk0OWU3Zjk1MzNhMjY4MTEwOTE4NmFmMzRlNGM3NCIsInN1YiI6IjY1ZmFmMTUzYmYzMWYyMDE3ZWZkYmIzZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OTEDpN4LbRZEnH0I1xxBD47TDc7o9CkL9qPqGOmIegs'
+ 
 
- // URL do endpoint para obter filmes populares
- const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+ // URL do endpoint para obter filmes 
+ const apiPopularUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+ 
+
+ 
 
  // Função para buscar e exibir filmes populares
  async function fetchPopularMovies() {
    try {
-     const response = await fetch(apiUrl);
+     const response = await fetch(apiPopularUrl);
      const data = await response.json();
 
     console.log(data)
@@ -20,7 +25,11 @@
      data.results.forEach(movie => {
        const listItem = document.createElement('li');
        listItem.innerText = movie.title;
+       const img = document.createElement('img');
+       const poster = movie.poster_path;
+       img.src = `https://image.tmdb.org/t/p/w500${poster}`
        movieList.appendChild(listItem);
+       movieList.appendChild(img);
 
      });
    } catch (error) {
@@ -28,7 +37,42 @@
    }
  }
 
- 
+function fetchSearchMovie(){
+    let filme;
+    const listSearch = document.getElementById('movies');
+    filme = listSearch.value;
+    const apiSearchMovie = `https://api.themoviedb.org/3/search/movie?query=${filme}&include_adult=false`
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Mzk0OWU3Zjk1MzNhMjY4MTEwOTE4NmFmMzRlNGM3NCIsInN1YiI6IjY1ZmFmMTUzYmYzMWYyMDE3ZWZkYmIzZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OTEDpN4LbRZEnH0I1xxBD47TDc7o9CkL9qPqGOmIegs'
+        }
+      };
+    fetch(apiSearchMovie, options)
+        .then(response => response.json())
+        .then(data => {
+            // Limpa a lista antes de adicionar novos filmes
+            const movieList = document.getElementById('movieList');
+            movieList.innerHTML = '';
+
+            // Itera sobre os resultados e adiciona à lista
+            data.results.forEach(movie => {
+                const list = document.createElement('li');
+                list.textContent = movie.title;
+                const img = document.createElement('img');
+                const poster = movie.poster_path;
+                img.src = `https://image.tmdb.org/t/p/w500${poster}`
+                movieList.appendChild(list);
+                movieList.appendChild(img)
+
+
+            });
+            console.log(data)
+        })
+        
+        .catch(err => console.error(err));
+}
 
  // Chamar a função para buscar e exibir filmes populares ao carregar a página
- window.onload = fetchPopularMovies;
+window.onload = fetchPopularMovies();
